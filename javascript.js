40b2338aa36c139d555bb07f1bc8273dc7ad58d3
Callistus <khalioris@gmail.com>
2022-06-24 00:03:59 +0100

    zuri task

---
+++ javascript.js
@@ -0,0 +1,9 @@
+var name = "Igbafe Callistus";
+console.log("My name is " + name);
+alert("my name is " + name);
+var height = "7.1 ft ";
+console.log("I am " + height + "in height");
+alert("I am " + height + "in height");
+var country = "Nigeria";
+console.log("I am from " + country);
+alert("I am from  " + country);
